7201029856f266fa7cdb6a088e752ab422a602a3
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-23 01:01:08 +0300

    Добавлен класс FileSystem, задание 8.1.4

---
+++ Module8.Library/FileSystem.cs
@@ -0,0 +1,16 @@
+﻿namespace Module8.Library
+{
+    public class FileSystem
+    {        
+        public char NameDisk { get; }
+        public long Sizedisk { get; }
+        public long FreeSpaceDisk { get; }
+
+        public FileSystem(char namedisk, long sizedisk, long freespacedisk)
+        {
+            this.NameDisk = namedisk;
+            this.Sizedisk = sizedisk;
+            this.FreeSpaceDisk = freespacedisk;
+        }
+    }
+}
