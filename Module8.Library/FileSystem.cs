889da00613b684730f17928d9fd9b778e76b4f09
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-23 01:12:39 +0300

    Добавлена возможность создания папок, задание 8.1.5

--- Module8.Library/FileSystem.cs
+++ Module8.Library/FileSystem.cs
@@ -5,12 +5,18 @@
         public char NameDisk { get; }
         public long Sizedisk { get; }
         public long FreeSpaceDisk { get; }
+        public List<Folder> Folders { get; }
 
         public FileSystem(char namedisk, long sizedisk, long freespacedisk)
         {
             this.NameDisk = namedisk;
             this.Sizedisk = sizedisk;
             this.FreeSpaceDisk = freespacedisk;
+        }
+
+        public void AddFolder(string namefolder)
+        {
+            Folders.Add(new Folder(namefolder));
         }
     }
 }
---
+++ Module8.Library/Folder.cs
@@ -0,0 +1,13 @@
+﻿namespace Module8.Library
+{
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+        public string Name { get; }
+
+        public Folder(string name)
+        {
+            Name = name;
+        }
+    }
+}
