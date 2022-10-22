247d6ce47108076193f2a5a43f29c8998642feec
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-23 01:31:21 +0300

    Создан метод подсчитывающий файлы и каталоги, задание 8.2.1

--- Module8/Program.cs
+++ Module8/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GetCatalogsCount(); //   Вызов метода получения директорий
+        }
+
+        static void GetCatalogs()
+        {
+            string dirName = @"C:\\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine(d);
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+            }
+        }
+
+        static void GetCatalogsCount()
+        {
+            string dirName = @"C:\\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                Console.WriteLine(dirs.Length); // Количество каталогов
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+
+                Console.WriteLine(files.Length); // Количество файлов
+            }
         }
     }
 }
