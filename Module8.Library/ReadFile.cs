e98c804e32f5e3540e06f34598ca30c76ebe63cf
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-24 22:22:40 +0300

    Вывод текста программы в консоль, задание 8.3.1

---
+++ Module8.Library/ReadFile.cs
@@ -0,0 +1,25 @@
+﻿namespace Module8.Library
+{
+    public class ReadFile
+    {
+        string FilePath;
+
+        public ReadFile(string filepath)
+        {
+            FilePath = filepath;
+        }
+
+        public void Read()
+        {            
+            // Откроем файл и прочитаем его содержимое
+            using (StreamReader sr = File.OpenText(FilePath))
+            {
+                string str = "";
+                while ((str = sr.ReadLine()) != null) // Пока не кончатся строки - считываем из файла по одной и выводим в консоль
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
---
+++ Task8.3.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Module8.Library;
+
+namespace Task8_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFile rf = new ReadFile(@"D:\Андрей\Программирование\C#\SF\Module8\Module8.Library\ReadFile.cs");
+            rf.Read();
+            Console.ReadKey();
+        }
+    }
+}
