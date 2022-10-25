29702ec477c44d834143b5e6d35a78f37a4905c6
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-25 21:17:02 +0300

    Добавлено задание 8.4.1

---
+++ Task8.4.1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task8_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filename = "BinaryFile.bin";
+
+            Console.WriteLine("Из файла считано:");
+
+            Console.WriteLine(ReadValues(path + @"\" + filename));
+
+            WriteValue(path + @"\" + filename);
+        }
+
+        static string ReadValues(string path)
+        {
+            string str = "";
+
+            if (File.Exists(path))
+            {
+                // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open.
+                using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+                {
+                    // Применяем специализированные методы Read для считывания соответствующего типа данных.                    
+                    str = reader.ReadString();
+                }
+            }
+            return str;
+        }
+
+        static void WriteValue(string path)
+        {
+            if (File.Exists(path))
+            {
+                // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open.
+                using (BinaryWriter write = new BinaryWriter(File.Open(path, FileMode.Open)))
+                {
+                    // Применяем специализированные методы Read для считывания соответствующего типа данных.                    
+                    write.Write($"Файл изменен {DateTime.Now} на компьютере Windows 10");
+                }
+            }
+        }
+    }
+}
