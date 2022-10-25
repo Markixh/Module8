88ca8326a4d8ad051544504ef82677b633fed0d2
Марков Андрей <Andrejmarko@yandex.ru>
2022-10-25 21:32:08 +0300

    Добавлено задание 8.4.3

---
+++ Task8.4.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using Module8.Library;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+
+namespace Task8_4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact contact = new("Андрей", 91921995, "Andr@ru");
+            Console.WriteLine("Объект создан");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            using (var fs = new FileStream("myContact.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, contact);
+                Console.WriteLine("Объект сериализован");
+            }
+            // десериализация
+            using (var fs = new FileStream("myContact.dat", FileMode.Open))
+            {
+                var newContact = (Contact)formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"Имя: {newContact.Name} --- Телефон: {newContact.PhoneNumber} --- Email: {newContact.Email}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
